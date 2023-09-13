23d82075800ad38630c8aa8470510ebb17239ee1
mmthiyan021 <mmthiyan021@student.wethinkcode.co.za>
2023-09-14 01:25:40 +0200

    created simple post request

--- templateSpring/templateSpring/src/main/java/com/Template/templateSpring/Controller/SignUpController.java
+++ templateSpring/templateSpring/src/main/java/com/Template/templateSpring/Controller/SignUpController.java
@@ -1,10 +1,15 @@
 package com.Template.templateSpring.Controller;
 
+import com.Template.templateSpring.Dto.UserSignUpDto;
 import com.Template.templateSpring.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import com.Template.templateSpring.Entity.User;
+//@RequestMapping(path = "api", produces = MediaType.APPLICATION_JSON_VALUE)
+// For simplicity of this sample, allow all origins. Real applications should configure CORS for their use case.
 
 @RestController
 @RequestMapping("/calendar")
@@ -13,5 +18,17 @@ public class SignUpController {
     @Autowired
     private UserRepository userRepository;
 
-//    @PostMapping("/signup")
+    @PostMapping( "/signup")
+    public ResponseEntity<?> signUpUSer(@RequestBody UserSignUpDto userSignUpDto){
+        if(userRepository.existsByEmail(userSignUpDto.getEmail())){
+            return new ResponseEntity<>("User already exists", HttpStatus.BAD_REQUEST);
+        }
+        User user = new User();
+        user.setEmail(userSignUpDto.getEmail());
+        user.setPassword(userSignUpDto.getPassword());
+
+        userRepository.save(user);
+
+        return new ResponseEntity<>("User is registered successfully!", HttpStatus.OK);
+    }
 }
