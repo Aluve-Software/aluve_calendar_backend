ea68084418538809f058217c44db80c70a0f0e68
mmthiyan021 <mmthiyan021@student.wethinkcode.co.za>
2023-09-16 01:02:35 +0200

    added edited variables verification

--- templateSpring/templateSpring/src/main/java/com/Template/templateSpring/service/UserSignUpService.java
+++ templateSpring/templateSpring/src/main/java/com/Template/templateSpring/service/UserSignUpService.java
@@ -73,7 +73,7 @@ public class UserSignUpService implements UserService {
         mailMessage.setTo(user.getEmail());
         mailMessage.setSubject("Complete Registration!");
         mailMessage.setText("To confirm your account, please click here : "
-                +"http://localhost:8080/confirm-account?token="+confirmationToken.getConfirmationToken());
+                +"http://localhost:8089/confirm-account?token="+confirmationToken.getConfirmationToken());
         emailService.sendEmail(mailMessage);
 
         System.out.println("Confirmation Token: " + confirmationToken.getConfirmationToken());
