9b70e24253e4e03d030d04a5d47ab9bfc6ff3c62
Sibonelo <sibonelo@aluvesqe.co.za>
2023-10-12 11:06:36 +0200

    CORS implementation

--- templateSpring/templateSpring/src/main/java/tech/aluve/calendar/controller/LoginController.java
+++ templateSpring/templateSpring/src/main/java/tech/aluve/calendar/controller/LoginController.java
@@ -8,6 +8,7 @@ import tech.aluve.calendar.dto.LoginDTO;
 import tech.aluve.calendar.interfaces.OauthUserService;
 import tech.aluve.calendar.repository.UserRepository;
 
+@CrossOrigin
 @Controller
 @RequestMapping("/login")
 public class LoginController {
--- templateSpring/templateSpring/src/main/java/tech/aluve/calendar/controller/SignUpController.java
+++ templateSpring/templateSpring/src/main/java/tech/aluve/calendar/controller/SignUpController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/calendar")
 public class SignUpController {
